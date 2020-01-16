ef6d54804207a81943fd7814cead5c83fa5ab7f0
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-16 16:46:34 +0800

    add logo

--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { TopNav } from "./top-nav/top-nav";
+import logo from "../assets/logo.png";
+import styles from "./landing-page.module.css";
 
 export function LandingPage() {
   return (
     <div>
       <TopNav />
+      <img src={logo} className={styles.logo} alt="logo" />
     </div>
   );
 }

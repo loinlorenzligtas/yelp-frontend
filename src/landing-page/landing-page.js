a56e69427be669d3a30be892d72201057419a805
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-16 17:35:18 +0800

    add search bar

--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -2,12 +2,14 @@ import React from "react";
 import { TopNav } from "./top-nav/top-nav";
 import logo from "../assets/logo.png";
 import styles from "./landing-page.module.css";
+import { SearchBar } from "../search-bar/search-bar";
 
 export function LandingPage() {
   return (
     <div>
       <TopNav />
       <img src={logo} className={styles.logo} alt="logo" />
+      <SearchBar />
     </div>
   );
 }

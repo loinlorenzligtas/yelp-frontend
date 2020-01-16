a0590f456d1024b9ae8fc429c83aa5eb8b9b226d
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-16 16:29:27 +0800

    add top nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { LandingPage } from "./landing-page/landing-page";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return <LandingPage />;
 }
 
 export default App;
---
+++ src/landing-page/landing-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopNav } from "./top-nav/top-nav";
+
+export function LandingPage() {
+  return (
+    <div>
+      <TopNav />
+    </div>
+  );
+}

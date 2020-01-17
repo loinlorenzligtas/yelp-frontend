a1eb6a503f76871c52f1d2fcd0ef256a3885c358
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-17 10:58:57 +0800

    add search suggestions

--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -3,6 +3,7 @@ import { TopNav } from "./top-nav/top-nav";
 import logo from "../assets/logo.png";
 import styles from "./landing-page.module.css";
 import { SearchBar } from "../search-bar/search-bar";
+import { SearchSuggestions } from "./search-suggestions/search-suggestions";
 
 export function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export function LandingPage() {
       <TopNav />
       <img src={logo} className={styles.logo} alt="logo" />
       <SearchBar />
+      <SearchSuggestions />
     </div>
   );
 }

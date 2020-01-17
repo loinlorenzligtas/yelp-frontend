cb962520de9dc581258252a2974f6a05536791ee
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-17 15:13:24 +0800

    add sub nav

--- src/search/search.js
+++ src/search/search.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { NavBar } from "../nav-bar/nav-bar";
+import { SubNav } from "../nav-bar/sub-nav/sub-nav";
 
 export function Search() {
   return (
     <div>
       <NavBar />
+      <SubNav />
     </div>
   );
 }

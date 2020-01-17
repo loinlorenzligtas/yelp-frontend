994d935a001eaab339dae0f7741b1904cefce866
loinlorenz.ligtas <loinlorenz.ligtas@gmail.com>
2020-01-17 13:58:36 +0800

    add nav bar

--- src/search/search.js
+++ src/search/search.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { NavBar } from "../nav-bar/nav-bar";
 
 export function Search() {
-  return <div>Search</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
